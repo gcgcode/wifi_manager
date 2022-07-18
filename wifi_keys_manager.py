cf293b8d4b46c31d1c134c439e6cd3699b4c0ec9
gcgcode <57359637+gcgcode@users.noreply.github.com>
2022-07-18 16:41:33 +0200

    WiFi manager final version

--- wifi_keys_manager.py
+++ wifi_keys_manager.py
@@ -1,14 +1,48 @@
-import os
-import sys
-import re
- 
-hi =  os.system('netsh wlan show profile | findstr "todos los usuarios"')
+import subprocess
 
-# Iterate over output to get each wifi SSID
+def inOutConsole():
+    print("Idiomas disponibles: \n" + 
+    "1. Español \n" +
+    "2. English \n" + 
+    "3. Exit")
+    lang = int(input("\nSelecciona un idioma:"))
 
-# Save SSID in a list
+    return lang
 
-# Ask to the user for SSID selection
+def getNetworkKey(filter1, filter2):
+    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')
+    profiles = [i.split(":")[1][1:-1] for i in data if filter1 in i]
+    
+    for i in profiles:
+        
+        try:
+            results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8', errors="backslashreplace").split('\n')
+            results = [b.split(":")[1][1:-1] for b in results if filter2 in b]
+            try:
+                print ("{:<30}|  {:<}".format(i, results[0]))
+            except IndexError:
+                print ("{:<30}|  {:<}".format(i, ""))
+        except subprocess.CalledProcessError:
+            print ("{:<30}|  {:<}".format(i, "ENCODING ERROR"))
+            
+lang = inOutConsole()
 
-# If exist, check for WiFi password > netsh wlan show profile name=Calculadora key=clear | findstr "clave"
+while lang != 3:
+    
+    if lang == 1:
+        filter1 = "todos los usuarios"
+        filter2 = "Contenido"
+        getNetworkKey(filter1, filter2)
+        break
+    elif lang == 2: 
+        filter1 = "All User Profile"
+        filter2 = "Key Content" 
+        getNetworkKey(filter1, filter2)
+        break
+    elif lang == 3:
+        break
+    else:
+        print("\nPlease, select a valid option")
+        lang = inOutConsole()
+
 
